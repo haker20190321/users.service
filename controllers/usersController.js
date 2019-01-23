a7a797c4728a3c40c7846b2fb300445ed241d2fd
kmayer <k.v.mayer@tm2.etagi.com>
2019-01-23 14:55:22 +0500

    interception of errors in the controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,35 +4,72 @@ const User = require('../service/usersService');
 module.exports = {
   /**
    * Create user
-   * @param params
-   * @param ext
+   * @param {Object} params
+   * @param {Object} ext
+   * @param {Function} writeError
    * @return {Promise<void>}
    */
-  createUser: async({userData}, ext) => await User.createUser(userData.value, ext),
+  createUser: async({userData}, ext, writeError) => {
+    try {
+      return await User.createUser(userData.value, ext);
+    } catch(error) {
+      return writeError(error.message);
+    }
+  },
   /**
    * Update user
    * @param params
-   * @param ext
-   */
-  updateUser: async({userId, userData}, ext) =>
-    await User.updateUser(userId.value, userData.value, ext),
-  /**
-   * Get user by id
-   * @param params
-   * @param ext
-   */
-  getUser: async({userId}, ext) => await User.getUser(userId.value, ext),
-  /**
-   * Delete user by id
-   * @param params
-   * @param ext
-   */
-  deleteUser: async({userId}, ext) => await User.deleteUser(userId.value, ext),
-  /**
-   * Search users
-   * @param params
-   * @param ext
+   * @param {Object} ext
+   * @param {Function} writeError
    * @return {Promise<void>}
    */
-  searchUsers: async({searchParams}, ext) => await User.searchUsers(searchParams.value, ext)
+  updateUser: async({userId, userData}, ext, writeError) => {
+    try {
+      return await User.updateUser(userId.value, userData.value, ext);
+    } catch(error) {
+      return writeError(error.message);
+    }
+  },
+  /**
+   * Get user by id
+   * @param {Object} params - parameters
+   * @param {Object} ext - extensions
+   * @param {Function} writeError - error callback
+   * @return {Promise<void>}
+   */
+  getUser: async({userId}, ext, writeError) => {
+    try {
+      return await User.getUser(userId.value, ext);
+    } catch(error) {
+      return writeError(error.message);
+    }
+  },
+  /**
+   * Delete user by id
+   * @param {Object} params - parameters
+   * @param {Object} ext - extensions
+   * @param {Function} writeError - error callback
+   * @return {Promise<void>}
+   */
+  deleteUser: async({userId}, ext, writeError) => {
+    try {
+      return await User.deleteUser(userId.value, ext);
+    } catch(error) {
+      return writeError(error.message);
+    }
+  },
+  /**
+   * Search users
+   * @param {Object} params
+   * @param {Object} ext
+   * @param {Function} writeError
+   * @return {Promise<void>}
+   */
+  searchUsers: async({searchParams}, ext, writeError) => {
+    try {
+      return await User.searchUsers(searchParams.value, ext);
+    } catch(error) {
+      return writeError(error.message);
+    }
+  }
 };
